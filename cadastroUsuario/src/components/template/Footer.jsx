52fb0edc5f6248b7e1d7851a3fc71299747a2f2a
Ramon Gomes <ramon_gds@hotmail.com>
2020-09-24 22:22:23 -0300

    Bloco de notas em React

--- cadastroUsuario/src/components/template/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Footer.css';
-
-export default props =>
-    <footer className="footer">
-        <span>
-            Desenvolvido por <i className="fa fa-heart text-danger"></i> Ramon
-        </span>
-    </footer>
